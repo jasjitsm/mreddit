9a3374420a9ef9de68ea2c5857cae9ad2a9b1a71
Jasjit <jasjitsm@gmail.com>
2017-12-11 20:40:57 +0530

    Integrated Firebase and AngularFire
    Installed Firebase and AngularFire2 and changed both prod and dev environments accordingly.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBLTZDTCeOQBg0rM8mHN3nS9eVsEYkYfuE",
+    authDomain: "mreddit-389fb.firebaseapp.com",
+    databaseURL: "https://mreddit-389fb.firebaseio.com",
+    projectId: "mreddit-389fb",
+    storageBucket: "",
+    messagingSenderId: "1060257430010"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBLTZDTCeOQBg0rM8mHN3nS9eVsEYkYfuE",
+    authDomain: "mreddit-389fb.firebaseapp.com",
+    databaseURL: "https://mreddit-389fb.firebaseio.com",
+    projectId: "mreddit-389fb",
+    storageBucket: "",
+    messagingSenderId: "1060257430010"
+  }
 };
