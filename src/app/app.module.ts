4868aa1b46c3ebfcb6a371b8238174fc933d47dc
Jasjit <jasjitsm@gmail.com>
2017-12-16 07:48:45 +0530

    Firebase Deployment
    Initialised and deployed the app to Firebase. NOTE: firebase.json may need to be altered once routing is complete, as it may cause interference.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
 import { SidebarHeaderComponent } from './sidebar/sidebar-header/sidebar-header.component';
 import { SidebarLinksComponent } from './sidebar/sidebar-links/sidebar-links.component';
 import { ArticleMainComponent } from './article/article-main/article-main.component';
-import { ArticleDescriptionComponent } from './article/article-description/article-description.component';
 import { CommentsListComponent } from './article/comments-list/comments-list.component';
 import { CommentsSingleComponent } from './article/comments-single/comments-single.component';
 
@@ -21,12 +22,12 @@ import { CommentsSingleComponent } from './article/comments-single/comments-sing
     SidebarHeaderComponent,
     SidebarLinksComponent,
     ArticleMainComponent,
-    ArticleDescriptionComponent,
     CommentsListComponent,
     CommentsSingleComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
